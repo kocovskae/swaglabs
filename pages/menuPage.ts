69dfaf41343a2cf77e84c8b8d8cdffd06df2ec88
elena.kostova <elena.kostova@avenga.com>
2025-10-31 09:20:19 +0100

    adding menu tests and PO

---
+++ pages/menuPage.ts
@@ -0,0 +1,26 @@
+import { Page, expect } from "@playwright/test"
+
+export class MenuPage {
+    readonly page: Page;
+    readonly menuIcon;
+    readonly menuList;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.menuIcon = this.page.locator("#react-burger-menu-btn");
+        this.menuList = this.page.locator(".bm-item-list a");
+    }
+
+
+    async verifyThatHamburegerMenuIsVisible() {
+        await expect(this.menuIcon).toBeVisible();
+    }
+
+    async clickOnHamburgerMenu() {
+        await this.menuIcon.click();
+    }
+
+    async countHamburgerMenuItems(menuItems: string[]) {
+        await expect(this.menuList).toHaveCount(menuItems.length);
+    }
+}
